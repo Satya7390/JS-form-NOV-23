c279adf60339978ab5df359948f77b90b3555356
satya <satyanandgupta845@gmail.com>
2023-11-03 03:53:45 +0530

    03-NOV

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 import React, { Component } from 'react'
+import ListItems from './ListItems';
+import { Library, library } from '@fortawesome/fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-export class App extends Component {
+library .add(faTrash);
+export class App extends React.Component {
   constructor(props) {
     super(props)
   
@@ -12,20 +16,63 @@ export class App extends Component {
        currentItem:{
         text:"",
         key:''
-       }
+       },
+    
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+  handleInput(e){
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    if (newItem.text !== "") {
+
+      const newItems = [...this.state.items, newItem];
+      this.setState({
+        items: newItems,
+        currentItem: {
+          text: '',
+          key: ''
+        }
+      })
     }
+  }
+  deleteItem(key){
+    const filterItems = this.state.items.filter(item =>
+      item.key !== key);
+      this.setState({
+        items:filterItems
+      })
   }
   render() {
     return (
       <div className='App'>
      <header>
-     <form id="to-do-form">
-      <input type="text" placeholder='Enter Message here..'/>
+     <form id="to-do-form" onSubmit={this.addItem}>
+      <input type="text" placeholder='Enter Message here..'
+        value={this.state.currentItem.text}
+        onChange={this.handleInput}
+      />
         <button type='submit'>
           ADD
         </button>
      </form>
       </header>
+      <ListItems items ={this.state.items}
+      deleteItem = {this.deleteItem}
+      >
+      </ListItems>
       </div>
     )
   }
